923133622674a5e4e5d755b531f577248796b482
pospi <sam@everledger.io>
2016-08-25 19:54:01 +1000

    added name scope handler function for postcss-modules generateScopedName option

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 /**
  * Webpack plugin for processing postcss modules as classname hash objects.
  *
+ * :CAVEAT: if app components use same class names as those of partials, they will end up global
+ *
  * @package: postcss-modules-component-plugin
  * @author:  pospi <sam@everledger.io>
  * @since:   2016-08-25
  */
+
+const loaderUtils = require('loader-utils');
 
 
 // JSON cache layer
@@ -32,6 +36,36 @@ function getLoaderName() {
 }
 
 
+// provide a function for handling name scoping...
+
+const SELECTORS_ENCOUNTERED_GLOBALLY = {};
+
+function scopedName(name, filename, css) {
+  // In component entrypoints:
+  // where we find selectors encountered in our partials, leave them alone
+  if (SELECTORS_ENCOUNTERED_GLOBALLY[name]) {
+    return name;
+  }
+
+  // In partials:
+  // use global selectors within node_modules folder
+  if (filename.match(/\/node_modules\//)) {
+    SELECTORS_ENCOUNTERED_GLOBALLY[name] = true;
+    return name;
+  } else if (SELECTORS_ENCOUNTERED_GLOBALLY[name]) {
+    SELECTORS_ENCOUNTERED_GLOBALLY[name] = false;
+  }
+
+  // In component entrypoints:
+  // use local selectors for app code by default
+  return loaderUtils.interpolateName(
+    { resourcePath: filename },
+    `[name][emoji]_${name}_[hash:base64:5]`,
+    { content: css }
+  );
+}
+
+
 // Main loader entrypoint (injects JSON onto `css-loader` output)
 
 function Loader(source) {
@@ -44,5 +78,6 @@ function Loader(source) {
 
 Loader.loader = getLoaderName;
 Loader.writer = setFileJSON;
+Loader.scopedName = scopedName;
 
 module.exports = Loader;
