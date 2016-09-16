c05d7aef1b05eb9fab6948c7d14e3d80a275c913
pospi <sam@everledger.io>
2016-09-16 12:55:22 +1000

    fix all classes from same file having same hashes/emojis

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function scopedName(name, filename, css) {
   return loaderUtils.interpolateName(
     { resourcePath: filename },
     localModuleNameFormat.replace(/\[localName\]/g, name),
-    { content: css }
+    { content: css.substring(css.indexOf('.' + name)) }
   );
 }
 
